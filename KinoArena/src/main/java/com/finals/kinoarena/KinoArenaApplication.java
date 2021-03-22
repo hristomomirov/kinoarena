ad12c4ea28de6f17e8b23c4fa549716519c5483e
Hristo Momirov.com <hristomomirov@gmail.com>
2021-03-22 15:08:25 +0200

    added project

---
+++ KinoArena/src/main/java/com/finals/kinoarena/KinoArenaApplication.java
@@ -0,0 +1,13 @@
+package com.finals.kinoarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoArenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoArenaApplication.class, args);
+    }
+
+}

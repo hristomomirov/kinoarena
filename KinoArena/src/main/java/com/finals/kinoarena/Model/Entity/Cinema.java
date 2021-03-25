0da2dc20d79b0ee8205d1d0425edf75b1eefddd8
Pavel Lyutov <pljutov@gmail.com>
2021-03-25 18:08:01 +0200

    same as last with just one fix on Cinema Entity

--- KinoArena/src/main/java/com/finals/kinoarena/Model/Entity/Cinema.java
+++ KinoArena/src/main/java/com/finals/kinoarena/Model/Entity/Cinema.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -27,5 +28,6 @@ public class Cinema {
     public Cinema(String name, String city) {
         this.name = name;
         this.city = city;
+        halls =new ArrayList<>();
     }
 }
